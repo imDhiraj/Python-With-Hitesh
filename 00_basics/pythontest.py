e9f9f6388a5c898f7fabde09cd101c92ef876866
Dhiraj <dhirajpk28@gmail.com>
2025-07-31 22:32:04 +0530

    comment the leftout  comments

--- 00_basics/pythontest.py
+++ 00_basics/pythontest.py
@@ -1,3 +1,7 @@
+# this packege sys lets us see what is the system related info 
 import sys
 
 print(sys.version)
+
+#Output
+# 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)]
