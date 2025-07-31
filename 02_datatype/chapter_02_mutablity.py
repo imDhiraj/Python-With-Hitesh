55ded22d1b438cd7b12947e07e8c5def3f33ad40
Dhiraj <dhirajpk28@gmail.com>
2025-07-31 22:36:25 +0530

    remove some spelling errors

--- 02_datatype/chapter_02_mutablity.py
+++ 02_datatype/chapter_02_mutablity.py
@@ -2,8 +2,8 @@ spice_mix=set()
 
 print(spice_mix)
 print(f"intial Id: {id(spice_mix)}")
-spice_mix.add("cardium")
-spice_mix.add("red chill")
+spice_mix.add("cardamom")
+spice_mix.add("red chilli")
 spice_mix.add("salt")
 
 print(spice_mix)
@@ -12,5 +12,5 @@ print(f"after change Id: {id(spice_mix)}")
 #### Output
 # set()
 # intial Id: 2103475765984             we can see even if we want to change the value in side the set it doestnt change it refaracne id 
-# {'red chill', 'salt', 'cardium'}
+# {'red chilli', 'salt', 'cardamom'}
 # after change Id: 2103475765984
